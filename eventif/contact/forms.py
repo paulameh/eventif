e17ff63d69b1a3cabae55a24b9dea163c4ba26c4
Breno <101750209+paulameh@users.noreply.github.com>
2024-12-02 15:50:51 -0300

    Delete eventif directory

--- eventif/contact/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class contactForm(forms.Form):
-    name = forms.CharField(label="Nome")
-    email = forms.EmailField(label="email")
-    phone = forms.CharField(label="Telefone", required=False)
-    message = forms.CharField(label="Mensagem", widget=forms.Textarea(attrs={"rows":"5"}))
--- eventif/subscriptions/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class SubscriptionForm(forms.Form):
-    name = forms.CharField(label="Nome")
-    cpf = forms.CharField(label="CPF")
-    email = forms.EmailField(label="email")
-    phone = forms.CharField(label="Telefone")
