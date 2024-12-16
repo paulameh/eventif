6617682844cae9e14d42041b7e981de47f3df351
Ben_XP <brexp14@gmail.com>
2024-12-16 14:22:27 -0300

    Atualização de speaker com models

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -30,3 +30,16 @@ class Hometest(TestCase):
             self.response, 'href="{}"'.format(r('subscriptions:new'))
         )
     
+    def test_speakers(self):
+        contents = [
+            'Grace Hopper',
+            'https://abre.ai/hopper-pic',
+            'Alan Turing',
+            'https://abre.ai/turing-pic'
+        ]
+        for expected in contents:
+            with self.subTest():
+                self.assertContains(self.response, expected)
+    def test_speakers_link(self):
+        expected = 'href="{}#speakers"'.format(r('home'))
+        self.assertContains(self.response, expected)
